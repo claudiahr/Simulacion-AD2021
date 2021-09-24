18e84bdfbcbf7d069e3060c1db3072cb9c921e7b
claudiahr <88215675+claudiahr@users.noreply.github.com>
2021-09-24 00:20:46 -0500

    Prueba con strings.

---
+++ HW5/Pruebas/decimales prueba 2.r
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(datos)
+library(htmlwidgets)
+
+numerock = 0.369258147
+numtry = 0.369258741
+n = seq(str_length(string2), 1)
+
+string1 = numerock
+string2 = numtry
+
+str_length(string1)
+str_length(string2)
+
+str_locate(string1, "string2")
+
